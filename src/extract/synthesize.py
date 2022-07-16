51f42978d8fe60e9333dd18503e8241c368001f4
bacor <mail@bascornelissen.nl>
2022-07-16 14:40:30 +0200

    binomial walk synthetic contours

--- src/extract/synthesize.py
+++ src/extract/synthesize.py
@@ -48,79 +48,49 @@ def sample(
     return distribution.rvs(size=1)[0]
 
 
-class MarkovSynthesizer:
-    def __init__(self, smoothing: Optional[int] = 1):
-        self.smoothing = smoothing
+class ContourSynthesizer:
+    contour_id_tmpl = "synth-{i:0>4}"
 
-    def estimate_initial_distribution(self, contours: np.array) -> scipy.stats.binom:
-        initials = contours[:, 0]
-        mean = np.round(initials.mean())
-        var = initials.var()
-        return binom(mean, var)
+    initial_dist = None
+
+    length_dist = None
 
     def estimate_length_distribution(self, unit_lengths: np.array):
         mu = unit_lengths.mean()
         return scipy.stats.poisson(mu)
 
-    def count_bigrams(
-        self, contours: np.array, count_repetitions: Optional[bool] = False
-    ) -> Counter:
-        counts = Counter()
-        for contour in contours:
-            for p1, p2 in zip(contour[:-1], contour[1:]):
-                if count_repetitions or p1 != p2:
-                    counts[(p1, p2)] += 1
-        return counts
+    def fit(self, *args):
+        raise NotImplemented
 
-    def estimate_transition_matrix(
-        self, contours: np.array
-    ) -> Tuple[np.array, np.array]:
-        domain = np.arange(contours.min(), contours.max())
-        transitions = np.zeros((len(domain), len(domain)))
-        bigrams = self.count_bigrams(contours)
-        for i, p1 in enumerate(domain):
-            for j, p2 in enumerate(domain):
-                transitions[i, j] = bigrams[(p1, p2)] + self.smoothing
-        transitions = transitions / transitions.sum(axis=1)[:, None]
-        transitions = pd.DataFrame(transitions, columns=domain, index=domain)
-        return domain, transitions
+    def generate_initial(self):
+        raise NotImplemented
 
-    def fit(self, contours, unit_lengths):
-        self.initial_distr = self.estimate_initial_distribution(contours)
-        self.length_distr = self.estimate_length_distribution(unit_lengths)
-        self.domain, self.transition_matrix = self.estimate_transition_matrix(contours)
+    def step(self, i: int, contour: np.array) -> float:
+        raise NotImplemented
 
     def generate(self):
-        # Sample an initial that is in the domain
-        initial = None
-        while initial is None:
-            candidate = sample(self.initial_distr)
-            if candidate in self.domain:
-                initial = candidate
-
-        # Construct contour
         length = max(3, sample(self.length_distr))
         contour = np.zeros(length)
-        contour[0] = initial
+        contour[0] = self.generate_initial()
         for i in range(1, length):
-            probs = self.transition_matrix.loc[contour[i - 1], :].values
-            contour[i] = np.random.choice(self.domain, size=1, p=probs)
-
+            contour[i] = self.step(i, contour)
         return contour
 
     def generate_dataset(
         self,
         num_contours: int,
         num_samples: Optional[int] = 50,
-        contour_id_tmpl="markov-{i:0>4}",
-    ) -> np.array:
+        random_state: int = 12345,
+    ) -> pd.DataFrame:
+        """"""
+        np.random.seed(random_state)
         contours = np.zeros((num_contours, num_samples))
         lengths = np.zeros((num_contours, 1))
         ids = []
         for i in range(num_contours):
             contour = Contour(self.generate())
             lengths[i] = len(contour)
-            ids.append(contour_id_tmpl.format(i=i))
+            ids.append(self.contour_id_tmpl.format(i=i))
             contours[i, :] = contour.interpolate(num_samples).pitches
 
         # Collect in dataframe
@@ -148,6 +118,83 @@ class MarkovSynthesizer:
         return df
 
 
+class MarkovSynthesizer(ContourSynthesizer):
+    contour_id_tmpl = "markov-{i:0>4}"
+
+    def __init__(self, smoothing: Optional[int] = 1):
+        self.smoothing = smoothing
+
+    def count_bigrams(
+        self, contours: np.array, count_repetitions: Optional[bool] = False
+    ) -> Counter:
+        counts = Counter()
+        for contour in contours:
+            for p1, p2 in zip(contour[:-1], contour[1:]):
+                if count_repetitions or p1 != p2:
+                    counts[(p1, p2)] += 1
+        return counts
+
+    def estimate_transition_matrix(
+        self, contours: np.array
+    ) -> Tuple[np.array, np.array]:
+        domain = np.arange(contours.min(), contours.max())
+        transitions = np.zeros((len(domain), len(domain)))
+        bigrams = self.count_bigrams(contours)
+        for i, p1 in enumerate(domain):
+            for j, p2 in enumerate(domain):
+                transitions[i, j] = bigrams[(p1, p2)] + self.smoothing
+        transitions = transitions / transitions.sum(axis=1)[:, None]
+        transitions = pd.DataFrame(transitions, columns=domain, index=domain)
+        return domain, transitions
+
+    def estimate_initial_distribution(self, contours: np.array) -> scipy.stats.binom:
+        initials = contours[:, 0]
+        mean = np.round(initials.mean())
+        var = initials.var()
+        return binom(mean, var)
+
+    def fit(self, contours, unit_lengths):
+        self.initial_distr = self.estimate_initial_distribution(contours)
+        self.length_distr = self.estimate_length_distribution(unit_lengths)
+        self.domain, self.transition_matrix = self.estimate_transition_matrix(contours)
+
+    def generate_initial(self):
+        initial = None
+        while initial is None:
+            candidate = sample(self.initial_distr)
+            if candidate in self.domain:
+                return candidate
+
+    def step(self, i: int, contour: np.array) -> float:
+        probs = self.transition_matrix.loc[contour[i - 1], :].values
+        return np.random.choice(self.domain, size=1, p=probs)[0]
+
+
+class BinomialWalkSynthesizer(ContourSynthesizer):
+    contour_id_tmpl = "binom-{i:0>4}"
+
+    def __init__(
+        self, n: int = 10, p: float = 0.5, min_pitch: int = 60, max_pitch: int = 84
+    ):
+        self.n = n
+        self.p = p
+        self.domain = np.arange(min_pitch, max_pitch + 1)
+
+    def fit(self, unit_lengths: np.array):
+        self.length_distr = self.estimate_length_distribution(unit_lengths)
+
+    def generate_initial(self):
+        return np.random.choice(self.domain, size=1)
+
+    def step(self, i: int, contour: np.array) -> np.array:
+        step = np.random.binomial(self.n, self.p) - self.n * self.p
+        step = min(max(-12, step), 12)
+        if (contour[i - 1] + step > 84) or (contour[i - 1] + step < 60):
+            return contour[i - 1] - step
+        else:
+            return contour[i - 1] + step
+
+
 def main():
     import argparse
     from ..clusterability import Dataset
@@ -156,24 +203,30 @@ def main():
         description="Generate a dataset of synthetic Markov contours"
     )
     parser.add_argument(
-        "dataset",
+        "synthesizer",
         type=str,
         help="The dataset on which to fit the Markov contours",
     )
     args = parser.parse_args()
 
-    dataset = Dataset(args.dataset)
+    dataset = Dataset("combined-phrase")
     contours = dataset.representation("pitch")
 
-    # Fit synthesizer and generate dataset
-    synthesizer = MarkovSynthesizer()
-    synthesizer.fit(contours, dataset.df["unit_length"])
+    # Fit synthesizer
+    if args.synthesizer == "markov":
+        synthesizer = MarkovSynthesizer()
+        synthesizer.fit(contours, dataset.df["unit_length"])
+    elif args.synthesizer == "binom":
+        synthesizer = BinomialWalkSynthesizer()
+        synthesizer.fit(dataset.df["unit_length"])
+
+    # Generate contours dataset
     synth_contours = synthesizer.generate_dataset(num_contours=5000)
 
     # Save
-    contours_fn = os.path.join(CONTOUR_DIR, f"markov-contours.csv.gz")
+    contours_fn = os.path.join(CONTOUR_DIR, f"{args.synthesizer}-contours.csv.gz")
     synth_contours.to_csv(contours_fn, compression="gzip")
-    subset_fn = os.path.join(CONTOUR_DIR, f"markov-contours-indices.json")
+    subset_fn = os.path.join(CONTOUR_DIR, f"{args.synthesizer}-contours-indices.json")
     store_shuffled_indices(synth_contours, subset_fn)
 
 
